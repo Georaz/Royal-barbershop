c8ecde475e05b73118bb5f71c7a94b5f409da324
Georaz <georaz@inbox.ru>
2021-09-05 11:37:40 +0300

    comments localStorage code

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
-import {combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
 import mainPageReducer from "./reducers/mainPageReducer";
 import officeReducer from "./reducers/officeReducer";
 import authReducer from "./reducers/authReducer";
+import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
     mainPageReducer,
@@ -10,10 +11,14 @@ const rootReducer = combineReducers({
 });
 
 let ls = window.localStorage;
-const store = createStore(rootReducer, JSON.parse(ls['store']));
+const store = createStore(rootReducer, applyMiddleware(thunk));
+ls['store'] = JSON.stringify(store.getState())
 
-store.subscribe(() => {
-    ls['store'] = JSON.stringify(store.getState())
-})
+//-------localStorage---------
+// const store = createStore(rootReducer, JSON.parse(ls['store']));
+//
+// store.subscribe(() => {
+//     ls['store'] = JSON.stringify(store.getState())
+// })
 
 export default store;
